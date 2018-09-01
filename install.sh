78961bd3f9088ad7c575f4554ef1eb05468a05c2
Daniel Puleri <daniel.puleri@gmail.com>
2018-09-01 17:26:58 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# # Update Homebrew recipes
+# brew update
+#
+# # Install all our dependencies with bundle (See Brewfile)
+# brew tap homebrew/bundle
+# brew bundle
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -irf $HOME/.zshrc
+ln -s $HOME/.dotfiles/zshrc.symlink $HOME/.zshrc
+
+# Removes .vimrc if it exists and then symlinks it
+rm -irf $HOME/.vimrc
+ln -s $HOME/.dotfiles/vimrc.symlink $HOME/.vimrc
+
+# Removes git configurations and then symlink mine
+rm -irf $HOME/.gitconfig
+ln -s $HOME/.dotfiles/gitconfig.symlink $HOME/.gitconfig
+rm -irf $HOME/.gitignore
+ln -s $HOME/.dotfiles/gitignore.symlink $HOME/.gitignore
